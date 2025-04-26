700bf1a87cf01f662f1ee5f7e3632f15e6054e29
daddia <jonathan@daddia.com>
2025-04-26 20:46:55 +1000

    chore: update dependencies and configuration for Tailwind CSS and Storybook

--- packages/dui/tailwind.config.ts
+++ packages/dui/tailwind.config.ts
@@ -1,10 +1,9 @@
 import type { Config } from 'tailwindcss';
 import sharedConfig from '@repo/tailwind-config';
 
-const config: Pick<Config, 'prefix' | 'presets' | 'content'> = {
-  content: ['./src/**/*.tsx'],
-  prefix: 'ui-',
-  presets: [sharedConfig],
+const config: Config = {
+  ...sharedConfig,
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
 };
 
 export default config;
