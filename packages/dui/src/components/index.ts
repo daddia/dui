4292405a8e46347867eb112a68bf678afd27cfa6
daddia <jonathan@daddia.com>
2025-04-27 20:59:57 +1000

    feat: expand component exports and introduce keyboard key constants

--- packages/dui/src/components/index.ts
+++ packages/dui/src/components/index.ts
@@ -4,6 +4,8 @@ export * from './Container';
 export * from './Grid';
 export * from './Flex';
 export * from './Stack';
+export * from './AspectRatio';
+export * from './ScrollArea';
 
 // Typography
 export * from './Text';
@@ -12,6 +14,39 @@ export * from './Paragraph';
 export * from './Link';
 
 // Forms & Inputs
-export * from './Button';
+export * from './Accordion';
 export * from './Checkbox';
 export * from './CheckboxGroup';
+export * from './Label';
+export * from './Input';
+export * from './InputGroup';
+export * from './InputIcon';
+export * from './TextArea';
+export * from './RadioGroup';
+export * from './Select';
+export * from './Slider';
+export * from './Switch';
+
+// Buttons & Controls
+export * from './Button';
+export * from './ButtonGroup';
+export * from './Toggle';
+export * from './ToggleGroup';
+
+// Navigation
+
+// Media & Icons
+export * from './Avatar';
+
+// Overlay & Feedback
+export * from './AlertDialog';
+export * from './ContextMenu';
+export * from './Dialog';
+export * from './Popover';
+export * from './ProgressCircular';
+export * from './ProgressLinear';
+
+// Data Display
+
+// Utilities
+export * from './Divider';
