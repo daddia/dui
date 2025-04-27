f9c3e583300ce999a411936bf84479bd8977bb25
daddia <jonathan@daddia.com>
2025-04-27 21:07:25 +1000

    fix: update import path for utility function in Divider component

--- packages/dui/src/components/Divider/Divider.tsx
+++ packages/dui/src/components/Divider/Divider.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { Slot } from '@radix-ui/react-slot';
 import { dividerVariants } from './Divider.styles';
 import { DividerProps } from './Divider.types';
-import { cn } from '@/lib/utils';
+import { cn } from '../../utils/cn';
 
 export const Divider = React.forwardRef<HTMLDivElement, DividerProps>(
   (
